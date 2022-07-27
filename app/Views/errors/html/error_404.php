afbdcfc8977f6871bef2946eb3256afb79bb883b
Bagus Dwi <bagus.dwi@if.uai.ac.id>
2022-07-27 16:50:48 +0700

    new error page

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
 	<title>404 Page Not Found</title>
@@ -15,13 +16,16 @@
 			left: 50%;
 			margin-left: -73px;
 		}
+
 		body {
+			background-image: url('/assets/img/Artboard 1.png');
+			background-size: cover;
 			height: 100%;
-			background: #fafafa;
 			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
 			color: #777;
 			font-weight: 300;
 		}
+
 		h1 {
 			font-weight: lighter;
 			letter-spacing: 0.8;
@@ -30,6 +34,7 @@
 			margin-bottom: 0;
 			color: #222;
 		}
+
 		.wrap {
 			max-width: 1024px;
 			margin: 5rem auto;
@@ -37,13 +42,15 @@
 			background: #fff;
 			text-align: center;
 			border: 1px solid #efefef;
-			border-radius: 0.5rem;
+			border-radius: 10px;
 			position: relative;
 		}
+
 		pre {
 			white-space: normal;
 			margin-top: 1.5rem;
 		}
+
 		code {
 			background: #fafafa;
 			border: 1px solid #efefef;
@@ -51,9 +58,32 @@
 			border-radius: 5px;
 			display: block;
 		}
+
 		p {
 			margin-top: 1.5rem;
 		}
+
+		.btn {
+			display: inline-block;
+			text-align: center;
+			vertical-align: middle;
+			user-select: none;
+			border: 1px solid transparent;
+			transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+			text-transform: none;
+			font-weight: 600;
+			font-size: 12px;
+			line-height: 24px;
+			padding: .55rem 1.5rem;
+			letter-spacing: .5px;
+			border-radius: 30px;
+			box-shadow: 0 2px 6px #acb5f6;
+			background-color: #6777ef;
+			border-color: #6777ef;
+			cursor: pointer;
+			color: #fff;
+		}
+
 		.footer {
 			margin-top: 2rem;
 			border-top: 1px solid #efefef;
@@ -61,24 +91,21 @@
 			font-size: 85%;
 			color: #999;
 		}
+
 		a:active,
 		a:link,
 		a:visited {
-			color: #dd4814;
+			color: #fff;
 		}
 	</style>
 </head>
+
 <body>
 	<div class="wrap">
-		<h1>404 - File Not Found</h1>
-
-		<p>
-			<?php if (! empty($message) && $message !== '(null)') : ?>
-				<?= nl2br(esc($message)) ?>
-			<?php else : ?>
-				Sorry! Cannot seem to find the page you were looking for.
-			<?php endif ?>
-		</p>
+		<h1>We Are Under Construction</h1>
+		<p>The page you were looking for could not be found.</p>
+		<a class="btn btn-primary" href="home" role="button">Home</a>
 	</div>
 </body>
+
 </html>
